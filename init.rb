c33fd1d1c66e63f9729187dd7a120eff248795ee
Gary Taylor <garywork@dev-desktop.home>
2009-03-17 13:01:12 +0000

    Hopefully rails 2.3 compatible now

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 require File.dirname(__FILE__) + '/lib/ar_helpers'
 require File.dirname(__FILE__) + '/lib/fast_sessions'
 
-CGI::Session::ActiveRecordStore.session_class = CGI::Session::ActiveRecordStore::FastSessions
+ActiveRecord::SessionStore.session_class = CGI::Session::ActiveRecordStore::FastSessions
